9929b748859898d0cf134c982bf6b2afb9ac93c4
osaiz <osaiz@proyectoholdingvml.com>
2021-05-06 17:05:07 -0500

    Generamos modularidad, base de Datos y sus esquemas, estructura generar del fronted

---
+++ frontend/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div>
+      <b-table striped hover :items="items"></b-table>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' }
+      ]
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
